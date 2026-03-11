8b6cd6bbfd5c00003890d3e64c9da1cdb7899ef9
andresdino9ster@gmail.com <andresdino9ster@gmail.com>
2026-03-11 16:03:41 -0500

    Primer Commit, cargando proyecto

---
+++ src/main/java/com/example/crudfet/CrudfetApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudfet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudfetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudfetApplication.class, args);
+	}
+
+}
